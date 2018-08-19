ee9e53ee0011129b0548a0a21586d68e709be509
Write Code <writesoftcode@gmail.com>
2018-08-19 14:51:13 +0530

    initial commit of Market App with clean architecture

---
+++ B2BMarket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  B2BMarket
+//
+//  Created by Firoj Shaikh on 19/08/18.
+//  Copyright © 2018 Firoj Shaikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
